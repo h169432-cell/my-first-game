e6792a34412ae670c877ee9a863632ae0298e04b
h169432-cell <h169432@gmail.com>
2026-08-23 03:21:14 +0900

    Add individual evidence card image map

---
+++ assets/evidence-card-map.js
@@ -0,0 +1,13 @@
+window.CLUEVERGE_EVIDENCE_IMAGES = {
+  'motive-1': 'assets/cards/motive-1.webp',
+  'motive-2': 'assets/cards/motive-2.webp',
+  'alibi-vertical': 'assets/cards/alibi-1.webp',
+  'alibi-horizontal': 'assets/cards/alibi-2.webp',
+  'weapon-1': 'assets/cards/weapon-1.webp',
+  'weapon-2': 'assets/cards/weapon-2.webp',
+  'clue-1': 'assets/cards/clue-1.webp',
+  'clue-2': 'assets/cards/clue-2.webp',
+  'false-base': 'assets/cards/false-testimony.webp',
+  'false-special': 'assets/cards/false-testimony.webp',
+  'twist': 'assets/cards/twist.webp'
+};
